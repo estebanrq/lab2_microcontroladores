010525d5dfb609913946eaa63560ebc76738f60c
Esteban Rodriguez Quintana <erquintana0805@gmail.com>
2022-09-20 07:03:52 -0600

    updates diles

--- src/lab2.c
+++ src/lab2.c
@@ -1,10 +1,15 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+//#define  ONE_SEG  16000 // ~ un segundo para 16 MHz
+#define ONE_SEG  1000 // ~ un segundo para 1 MHz
+
+void pass_ped(int state);
+void pass_veh(int state);
+
+
 int main(void)
 {
-  //int ONE_SEG = 16000; // ~ un segundo para 16 MHz
-  int ONE_SEG = 1000; // ~ un segundo para 1 MHz
   DDRB = 0x1F; //Configuracion del puerto, B[0-5] como salida
 
 
@@ -16,3 +21,10 @@ int main(void)
     _delay_ms(ONE_SEG);
   }
 }
+
+void pass_ped(int state){
+  
+};
+void pass_veh(int state){
+  
+};
