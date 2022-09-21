fe95ac99a6bec1434d7b303f7a5356d7ff57d1ac
Esteban Rodriguez Quintana <erquintana0805@gmail.com>
2022-09-20 20:57:19 -0600

    updates lab 2 code. Adding global variables

--- src/lab2.c
+++ src/lab2.c
@@ -2,82 +2,90 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-//#define  ONE_SEG  16000 // ~ un segundo para 16 MHz
-#define ONE_SEG  1000 // ~ un segundo para 1 MHz
-//            B    43210
-#define Gv_Rp     0x06
-#define Rv_Gp     0x11
-#define Vy_blink  0b00001000
-#define Gp_blink  0b00000001
+//#define  ONE_SEG  16000 // ~ 1 s for 16 MHz
+#define ONE_SEG  1000 // ~ 1 s for 1 MHz
+#define CAR_PASS      0x06
+#define CAR_BLINK     0x02
+#define CAR_BLINK2    0x0A
+#define PED_PASS      0x11
+#define PED_BLINK     0x10
+#define PED_PASS_TIME 11*ONE_SEG
 
-void cars_pass(){
-  PORTB = 0x06;
-};
-void cars_no_pass(){
-  PORTB = 0x11;
-  _delay_ms(ONE_SEG*5);
-  PORTB = 0x10;
-  _delay_ms(ONE_SEG/2);
-  PORTB = 0x11;
-  _delay_ms(ONE_SEG/2);
-  PORTB = 0x10;
-  _delay_ms(ONE_SEG/2);
-  PORTB = 0x11;
-  _delay_ms(ONE_SEG/2);
-  PORTB = 0x10;
-  _delay_ms(ONE_SEG/2);
-  PORTB = 0x11;
-  _delay_ms(ONE_SEG/2);
-  PORTB = 0x10;
-  _delay_ms(ONE_SEG);
-
-};
-
-ISR (INT0_vect){  
-    _delay_ms(ONE_SEG/2);      
-    PORTB = 0x02;
-    _delay_ms(ONE_SEG/2);
-    PORTB = 0x0A;
-    _delay_ms(ONE_SEG/2);
-    PORTB = 0x02;
-    _delay_ms(ONE_SEG/2);
-    PORTB = 0x0A;
-    _delay_ms(ONE_SEG/2);    
-    PORTB = 0x02;
-    _delay_ms(ONE_SEG/2);
-    PORTB = 0x0A;
-    _delay_ms(ONE_SEG/2);
-    PORTB = 0x02;
-    _delay_ms(ONE_SEG);
-    cars_no_pass();
-}
-
-//ISR (PCINT0_vect){        // Interrupt service routine 
-//  PB5 < 1;    // Toggling the PB2 pin 
-//}
-
-void external_interrupt(){
-  //DDRB |= (1<<PB1)|(1<<PB5);     // set PB2 as output(LED)
-  sei();     //enabling global interrupt
-  GIMSK |= (1<<INT0);     // enabling the INT0 (external interrupt) 
-  MCUCR |= (1<<ISC01);    // Configuring as falling edge 
-}
-
-void pin_change_interrupt(){
-  DDRB|=(1<<PB0);
-  GIMSK|= (1<<PCIE0);
-  PCMSK|=(1<<PCINT1);
-}
-
+void cars_pass();
+void cars_blink();
+void pedestrian_blink();
+void cars_no_pass();
+void external_interrupt();
 
 ///// MAIN ///// 
 int main(void)
 {
-  DDRB = 0x1F; //Configuracion del puerto, B[0-5] como salida
+  DDRB = 0x1F; //configuring ports: B[0-5] as Output
   external_interrupt();
-  //pin_change_interrupt();
-  //Parpadear
   while (1) {
+    // cars will keep passing until botton pressed
     cars_pass();
   }
+}
+
+
+//// FUNCTIONS ////
+
+void cars_pass(){
+  PORTB = CAR_PASS;
+};
+
+void cars_blink(){
+  _delay_ms(ONE_SEG/2);   
+  PORTB = CAR_BLINK;
+  _delay_ms(ONE_SEG/2);
+  PORTB = CAR_BLINK2;
+  _delay_ms(ONE_SEG/2);
+  PORTB = CAR_BLINK;
+  _delay_ms(ONE_SEG/2);
+  PORTB = CAR_BLINK2;
+  _delay_ms(ONE_SEG/2);    
+  PORTB = CAR_BLINK;
+  _delay_ms(ONE_SEG/2);
+  PORTB = CAR_BLINK2;
+  _delay_ms(ONE_SEG/2);
+  PORTB = CAR_BLINK;
+  _delay_ms(ONE_SEG);
+};
+
+void pedestrian_blink(){
+  PORTB = PED_BLINK;
+  _delay_ms(ONE_SEG/2);
+  PORTB = PED_PASS;
+  _delay_ms(ONE_SEG/2);
+  PORTB = PED_BLINK;
+  _delay_ms(ONE_SEG/2);
+  PORTB = PED_PASS;
+  _delay_ms(ONE_SEG/2);
+  PORTB = PED_BLINK;
+  _delay_ms(ONE_SEG/2);
+  PORTB = PED_PASS;
+  _delay_ms(ONE_SEG/2);
+  PORTB = PED_BLINK;
+  _delay_ms(ONE_SEG);
+};
+void cars_no_pass(){
+  //pedestrians pass:
+  PORTB = PED_PASS;
+  _delay_ms(PED_PASS_TIME);
+  //blink pedestrians:
+  pedestrian_blink();
+};
+
+ISR (INT0_vect){  
+    //blink cars:
+    cars_blink();
+    // car no pass
+    cars_no_pass();
+}
+
+void external_interrupt(){
+  sei();     //enabling global interrupt
+  GIMSK |= (1<<INT0);     // enabling the INT0 (external interrupt) 
+  MCUCR |= (1<<ISC01);    // Configuring as falling edge
 }
